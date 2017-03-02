7918a12ee0c5f26f0816af11ff25465e08192257
David Knott <david1k1nott@gmail.com>
2017-03-02 00:48:50 -0700

    fix errors

--- sophia/settings.py
+++ sophia/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {}
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
